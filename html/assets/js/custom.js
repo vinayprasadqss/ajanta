ffe0804d535ceceaeb1c16a7db2239b1c1240592
divyakhuranaqss <divya@qsstechnosoft.com>
2024-06-13 18:36:10 +0530

    changes

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -9,3 +9,18 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 1, // Number of slides per view
+    spaceBetween: 10, // Space between each slide
+    loop: true, // Enable looping mode
+    
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
